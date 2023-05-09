e7c53ce48d14ac7124464b53096a46e185c42286
uripper <kripper11@gmail.com>
2023-05-09 15:10:46 -0600

    --- Auto Git Commit ---

--- rxToJSON.rb
+++ rxToJSON.rb
@@ -1,5 +1,19 @@
 require 'json'
 
+
+Dir.glob('rgss-stubs/lib/**/*.rb').each do |file|
+  begin
+    require_relative file
+  rescue => e
+    File.open('Logs/errors.log', 'a') do |f|
+      f.puts("Error loading file #{file}: #{e.message}")
+      f.puts(e.backtrace.join("\n"))
+      f.puts
+    end
+  end
+end
+
+
 Dir.glob('pokemon_essentials/pokemon-essentials/Data/Scripts/**/*.rb').each do |file|
   begin
     require_relative file
