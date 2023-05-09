6f600cec0a921bd49ac6d9d7538fbba46e33c1b8
uripper <36463273+uripper@users.noreply.github.com>
2023-05-08 19:34:06 -0600

    --- Auto Git Commit ---

--- src/controllers/reader.rs
+++ src/controllers/reader.rs
@@ -3,6 +3,39 @@ use std::io::{self, BufReader, Read};
 use std::any::Any;
 use std::collections::HashMap;
 
+
+#[derive(Clone)]
+struct Object {
+    class: String,
+    list: HashMap<String, Box<dyn Any>>,
+}
+
+#[derive(Clone)]
+struct Color {
+    red: f64,
+    green: f64,
+    blue: f64,
+    alpha: f64,
+}
+
+
+#[derive(Clone)]
+struct Table {
+    name: String,
+    color: Color,
+    width: i32,
+    height: i32,
+    data: Vec<Vec<String>>,
+}
+
+#[derive(Clone)]
+struct Tone {
+    red: f64,
+    green: f64,
+    blue: f64,
+    grey: f64,
+}
+
 struct Reader {
     file: BufReader<File>,
     object_cache: Vec<Box<dyn Any>>,
@@ -23,69 +56,106 @@ impl Reader {
         self.file.read_exact(&mut buf)?;
         let type_id = buf[0];
         match type_id {
-            b'@' => self.handle_link(),
-            b'I' => self.handle_ivar(),
-            b'e' => self.handle_extended(),
+            b'[' => self.handle_array(),
+            b'l' => self.handle_bignum(),
             b'C' => self.handle_class(),
-            b'0' => self.handle_nil(),
-            b'T' => self.handle_true(),
+            b'd' => self.handle_data(),
+            b'e' => self.handle_extended(),
             b'F' => self.handle_false(),
             b'i' => self.read_fixnum(),
             b'f' => self.handle_float(),
-            b'l' => self.handle_bignum(),
-            b'"' => self.handle_string(),
-            b'[' => self.handle_array(),
-            b'/' => self.handle_regexp(),
             b'{' => self.handle_hash(),
-            }
-            b'}' => self.handle_hash_end(),
+            b'}' => self.handle_hash_def(),
+            b'I' => self.handle_ivar(),
+            b'@' => self.handle_link(),
+            b'm' => self.handle_module(),
+            b'0' => self.handle_nil(),
+            b'/' => self.handle_regexp(),
+            b'"' => self.handle_string(),
+            b'S' => self.handle_struct(),
+            b':' => self.handle_symbol(),
+            b';' => self.handle_symlink(),
+            b'T' => self.handle_true(),
+            b'u' => self.handle_user_defined(),
+            b'U' => self.handle_user_marshall(),
+
             _ => Err(io::Error::new(io::ErrorKind::InvalidData, "Unknown type identifier")),
         }
     }
 
-    fn handle_link(&mut self) -> io::Result<Box<dyn Any>> {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    fn handle_bignum(&mut self) -> io::Result<Box<dyn Any>> {
         let mut buf = [0; 4];
         self.file.read_exact(&mut buf)?;
-        let index = u32::from_be_bytes(buf);
-        if index >= self.object_cache.len() as u32 {
-            return Err(io::Error::new(io::ErrorKind::InvalidData, "Invalid object index"));
+        let length = u32::from_be_bytes(buf);
+        let mut buf = vec![0; length as usize];
+        self.file.read_exact(&mut buf)?;
+        let mut result = 0;
+        for byte in buf {
+            result = result << 8 | byte as i64;
         }
-        Ok(self.object_cache[index as usize].clone())
+        Ok(Box::new(result))
     }
 
-    fn handle_ivar(&mut self) -> io::Result<Box<dyn Any>> {
-        let mut buf = [0; 4];
-        self.file.read_exact(&mut buf)?;
-        let index = u32::from_be_bytes(buf);
-        if index >= self.object_cache.len() as u32 {
-            return Err(io::Error::new(io::ErrorKind::InvalidData, "Invalid object index"));
-        }
-        let object = self.object_cache[index as usize].clone();
-        let object = object.downcast::<HashMap<Vec<u8>, Box<dyn Any>>>().unwrap();
-        let mut buf = [0; 4];
-        self.file.read_exact(&mut buf)?;
-        let index = u32::from_be_bytes(buf);
-        if index >= self.symbol_cache.len() as u32 {
-            return Err(io::Error::new(io::ErrorKind::InvalidData, "Invalid symbol index"));
-        }
-        let symbol = self.symbol_cache[index as usize].clone();
-        let mut buf = [0; 4];
-        self.file.read_exact(&mut buf)?;
-        let value = self.parse()?;
-        object.insert(symbol, value);
-        Ok(Box::new(object))
-    }
 
-    fn handle_extended(&mut self) -> io::Result<Box<dyn Any>> {
-        let mut buf = [0; 1];
-        self.file.read_exact(&mut buf)?;
-        let type_id = buf[0];
-        match type_id {
-            b'c' => self.handle_cache(),
-            b's' => self.handle_symbol(),
-            _ => Err(io::Error::new(io::ErrorKind::InvalidData, "Unknown extended type identifier")),
-        }
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     fn handle_class(&mut self) -> io::Result<Box<dyn Any>> {
         let mut buf = [0; 4];
@@ -108,17 +178,59 @@ impl Reader {
         Ok(Box::new(class))
     }
 
-    fn handle_nil(&mut self) -> io::Result<Box<dyn Any>> {
-        Ok(Box::new(()))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    fn handle_color(&mut self) -> io::Result<Box<dyn Any>> {
+        let mut color = Color { red: 0.0, green: 0.0, blue: 0.0, alpha: 0.0 };
+        self.file.read_exact(bytemuck::cast_slice_mut(&mut color))?;
+        Ok(Box::new(color))
+        }
+
+
+
+
+
+
+
+
+    fn handle_extended(&mut self) -> io::Result<Box<dyn Any>> {
+        let mut buf = [0; 1];
+        self.file.read_exact(&mut buf)?;
+        let type_id = buf[0];
+        match type_id {
+            b'c' => self.handle_cache(),
+            b's' => self.handle_symbol(),
+            _ => Err(io::Error::new(io::ErrorKind::InvalidData, "Unknown extended type identifier")),
+        }
     }
 
-    fn handle_true(&mut self) -> io::Result<Box<dyn Any>> {
-        Ok(Box::new(true))
-    }
+
+
 
     fn handle_false(&mut self) -> io::Result<Box<dyn Any>> {
         Ok(Box::new(false))
     }
+
+
 
     fn read_fixnum(&mut self) -> Result<i32, Box<dyn std::error::Error>> {
         // Read the first byte from the file
@@ -177,10 +289,439 @@ impl Reader {
         Ok(result)
     }
 
+
+
     fn handle_float(&mut self) -> io::Result<Box<dyn Any>> {
         let mut buf = [0; 8];
         self.file.read_exact(&mut buf)?;
         let float = f64::from_be_bytes(buf);
         Ok(Box::new(float))
     }
+
+
+    fn handle_ivar(&mut self) -> io::Result<Box<dyn Any>> {
+        let mut buf = [0; 4];
+        self.file.read_exact(&mut buf)?;
+        let index = u32::from_be_bytes(buf);
+        if index >= self.object_cache.len() as u32 {
+            return Err(io::Error::new(io::ErrorKind::InvalidData, "Invalid object index"));
+        }
+        let object = self.object_cache[index as usize].clone();
+        let object = object.downcast::<HashMap<Vec<u8>, Box<dyn Any>>>().unwrap();
+        let mut buf = [0; 4];
+        self.file.read_exact(&mut buf)?;
+        let index = u32::from_be_bytes(buf);
+        if index >= self.symbol_cache.len() as u32 {
+            return Err(io::Error::new(io::ErrorKind::InvalidData, "Invalid symbol index"));
+        }
+        let symbol = self.symbol_cache[index as usize].clone();
+        let mut buf = [0; 4];
+        self.file.read_exact(&mut buf)?;
+        let value = self.parse()?;
+        object.insert(symbol, value);
+        Ok(Box::new(object))
+    }
+
+
+
+
+    fn handle_link(&mut self) -> io::Result<Box<dyn Any>> {
+        let mut buf = [0; 4];
+        self.file.read_exact(&mut buf)?;
+        let index = u32::from_be_bytes(buf);
+        if index >= self.object_cache.len() as u32 {
+            return Err(io::Error::new(io::ErrorKind::InvalidData, "Invalid object index"));
+        }
+        Ok(self.object_cache[index as usize].clone())
+    }
+
+
+
+
+    fn handle_nil(&mut self) -> io::Result<Box<dyn Any>> {
+        Ok(Box::new(()))
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    fn handle_true(&mut self) -> io::Result<Box<dyn Any>> {
+        Ok(Box::new(true))
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    fn handle_string(&mut self) -> io::Result<Box<dyn Any>> {
+        let mut buf = [0; 4];
+        self.file.read_exact(&mut buf)?;
+        let length = u32::from_be_bytes(buf);
+        let mut buf = vec![0; length as usize];
+        self.file.read_exact(&mut buf)?;
+        Ok(Box::new(buf))
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    fn handle_array(&mut self) -> io::Result<Box<dyn Any>> {
+        let mut buf = [0; 4];
+        self.file.read_exact(&mut buf)?;
+        let length = u32::from_be_bytes(buf);
+        let mut array = Vec::new();
+        for _ in 0..length {
+            let value = self.parse()?;
+            array.push(value);
+        }
+        Ok(Box::new(array))
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    fn handle_regexp(&mut self) -> io::Result<Box<dyn Any>> {
+
+        let mut buf = [0; 4];
+        self.file.read_exact(&mut buf)?;
+        let length = u32::from_be_bytes(buf);
+        let mut buf = vec![0; length as usize];
+        self.file.read_exact(&mut buf)?;
+        let mut buf = [0; 4];
+        self.file.read_exact(&mut buf)?;
+        let options = u32::from_be_bytes(buf);
+        let mut regexp = HashMap::new();
+        regexp.insert(b"source".to_vec(), Box::new(buf));
+        regexp.insert(b"options".to_vec(), Box::new(options));
+        Ok(Box::new(regexp))
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    fn handle_hash(&mut self) -> io::Result<Box<dyn Any>> {
+        let mut buf = [0; 4];
+        self.file.read_exact(&mut buf)?;
+        let length = u32::from_be_bytes(buf);
+        let mut hash = HashMap::new();
+        for _ in 0..length {
+            let key = self.parse()?;
+            let value = self.parse()?;
+            hash.insert(key, value);
+        }
+        Ok(Box::new(hash))
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    fn handle_hash_default(&mut self) -> io::Result<Box<dyn Any>> {
+        let mut hash = HashMap::new();
+        let default = self.parse()?;
+        hash.insert(b"default".to_vec(), default);
+        Ok(Box::new(hash))
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    fn handle_struct(&mut self) -> io::Result<Box<dyn Any>> {
+        let mut buf = [0; 4];
+        self.file.read_exact(&mut buf)?;
+        let length = u32::from_be_bytes(buf);
+        let mut struct_ = HashMap::new();
+        for _ in 0..length {
+            let key = self.parse()?;
+            let value = self.parse()?;
+            struct_.insert(key, value);
+        }
+        Ok(Box::new(struct_))
+    }
+
+
+
+fn handle_data(&mut self) -> io::Result<Box<dyn Any>> {
+    let mut buf = [0; 4];
+    self.file.read_exact(&mut buf)?;
+    let length = u32::from_be_bytes(buf);
+    let mut buf = vec![0; length as usize];
+    self.file.read_exact(&mut buf)?;
+    Ok(Box::new(buf))
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+fn handle_module(&mut self) -> io::Result<Box<dyn Any>> {
+    let mut buf = [0; 4];
+    self.file.read_exact(&mut buf)?;
+    let length = u32::from_be_bytes(buf);
+    let mut buf = vec![0; length as usize];
+    self.file.read_exact(&mut buf)?;
+    Ok(Box::new(buf))
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+fn handle_symbol(&mut self) -> io::Result<Box<dyn Any>> {
+    let mut buf = [0; 4];
+    self.file.read_exact(&mut buf)?;
+    let length = u32::from_be_bytes(buf);
+    let mut buf = vec![0; length as usize];
+    self.file.read_exact(&mut buf)?;
+    Ok(Box::new(buf))
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+fn handle_symlink(&mut self) -> io::Result<Box<dyn Any>> {
+    let mut buf = [0; 4];
+    self.file.read_exact(&mut buf)?;
+    let length = u32::from_be_bytes(buf);
+    let mut buf = vec![0; length as usize];
+    self.file.read_exact(&mut buf)?;
+    Ok(Box::new(buf))
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+fn handle_tone(&mut self) -> io::Result<Box<dyn Any>> {
+    fn handle_table(&mut self) -> io::Result<Box<dyn Any>> {
+    let mut table = Table { x: 0, y: 0, width: 0, height: 0 };
+    self.file.read_exact(bytemuck::cast_slice_mut(&mut table))?;
+    Ok(Box::new(table))
+    }
+
+    let mut tone = Tone { red: 0.0, green: 0.0, blue: 0.0, gray: 0.0 };
+    self.file.read_exact(bytemuck::cast_slice_mut(&mut tone))?;
+    Ok(Box::new(tone))
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+fn handle_user_defined(&mut self) -> io::Result<Box<dyn Any>> {
+            let type_name_as_bytes = self.parse()?;
+            let type_name = match type_name_as_bytes.downcast_ref::<Vec<u8>>() {
+                Some(vec) => String::from_utf8_lossy(vec).into_owned(),
+                None => return Err(io::Error::new(io::ErrorKind::InvalidData, "UserDef name not Symbol")),
+            };
+
+            match type_name.as_str() {
+                "Color" => self.handle_color(),
+                "Table" => self.handle_table(),
+                "Tone" => self.handle_tone(),
+                _ => Err(io::Error::new(io::ErrorKind::InvalidData, format!("Unsupported user defined class: {}", type_name))),
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+fn handle_user_marshall(&mut self) -> io::Result<Box<dyn Any>> {
+    let mut buf = [0; 4];
+    self.file.read_exact(&mut buf)?;
+    let length = u32::from_be_bytes(buf);
+    let mut buf = vec![0; length as usize];
+        self.file.read_exact(&mut buf)?;
+        Ok(Box::new(buf))
+    }
+
+
+
 }
