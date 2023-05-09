8ee11959f733141c4b261a8c2579169b8f32f0d9
uripper <36463273+uripper@users.noreply.github.com>
2023-05-09 04:56:53 -0600

    --- Auto Git Commit ---

--- src/controllers/reader.rs
+++ src/controllers/reader.rs
@@ -1,8 +1,6 @@
-use nom::{number::streaming::be_u8, IResult, number::streaming::be_i32};
-use std::fs::File;
-use std::io::Read;
+use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Deserialize, Serialize)]
 pub enum ParsedValue {
     Link(i32),
     IVar(String),
@@ -18,115 +16,44 @@ pub enum ParsedValue {
     Symbol(Vec<u8>),
 }
 
-fn parser_caller(input: &[u8]) -> IResult<&[u8], ParsedValue> {
-    let (input, type_byte) = be_u8(input)?;
-
-    match type_byte {
-        b'@' => unimplemented!("parse_link"),
-        b'I' => unimplemented!("parse_ivar"),
-        b'0' => unimplemented!("parse_nil"),
-        b'T' => unimplemented!("parse_true"),
-        b'F' => unimplemented!("parse_false"),
-        b'i' => unimplemented!("parse_fixnum"),
-        b'f' => unimplemented!("parse_float"),
-        b'l' => unimplemented!("parse_bignum"),
-        b'"' => unimplemented!("parse_string"),
-        b'[' => unimplemented!("parse_array"),
-        b'{' => unimplemented!("parse_hash"),
-        b'u' => unimplemented!("parse_userdef"),
-        b'o' => unimplemented!("parse_object"),
-        b':' => unimplemented!("parse_symbol"),
-        b';' => unimplemented!("parse_symlink"),
-        b'e' => unimplemented!("parse_extended"),
-        b'C' => unimplemented!("parse_uclass"),
-        b'S' => unimplemented!("parse_struct"),
-        b'U' => unimplemented!("parse_usermarshal"),
-        b'd' => unimplemented!("parse_data"),
-        b'M' => unimplemented!("parse_moduleold"),
-        b'c' => unimplemented!("parse_class"),
-        b'm' => unimplemented!("parse_module"),
-        _ => Err(nom::Err::Failure(nom::error::Error::new(input, nom::error::ErrorKind::Eof)))
-,
-    }
+fn parse_value(input: &[u8]) -> Result<ParsedValue, Box<dyn std::error::Error>> {
+    let value = bincode::deserialize(input)?;
+    Ok(value)
 }
 
-fn parser_scanner(file_path: &str) -> Result<(), Box<dyn std::error::Error>> {
-    // Open the file
-    let mut file = File::open(file_path)?;
+fn parse_file(file_path: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let data = std::fs::read(file_path)?;
 
-    // Read the file into a byte vector
-    let mut data = Vec::new();
-    file.read_to_end(&mut data)?;
+    let mut remaining_data = &data[..];
+    let mut object_cache = Vec::new();
 
-    // Call the parser_caller function
-    let result = parser_caller(&data);
+    while !remaining_data.is_empty() {
+        let parsed_value = parse_value(remaining_data)?;
+        remaining_data = &remaining_data[parsed_value_size(&parsed_value)..];
 
-    match result {
-        Ok((remaining_data, parsed_value)) => {
-            // Handle the parsed value
-            println!("Parsed value: {:?}", parsed_value);
+        match parsed_value {
+            ParsedValue::Link(index) => {
+                let index = index - 1;
 
-            // If there's any data left, you could continue parsing here
-            if !remaining_data.is_empty() {
-                println!("There's more data to parse!");
+                if index < 0 || index as usize >= object_cache.len() {
+                    return Err("Invalid link index".into());
+                }
+
+                let object = object_cache[index as usize].clone();
+                object_cache.push(object);
+            }
+            _ => {
+                object_cache.push(parsed_value);
             }
         }
-        Err(err) => {
-            // Handle the error
-            println!("An error occurred: {:?}", err);
-        }
+
+        println!("Parsed value: {:?}", parsed_value);
     }
 
     Ok(())
 }
 
-fn parse_link<'a>(input: &'a [u8], object_cache: &'a Vec<ParsedValue>) -> IResult<&'a [u8], ParsedValue> {
-    let (input, index) = be_i32(input)?;
-
-    // Subtract 1 from the index because the C++ code does so
-    let index = index - 1;
-
-    // Check if the index is valid
-    if index < 0 || index as usize >= object_cache.len() {
-        return Err(nom::Err::Failure(nom::error::Error::new(input, nom::error::ErrorKind::Eof)));
-    }
-
-    // Retrieve the object from the cache
-    let object = object_cache[index as usize].clone();
-
-    Ok((input, object))
-}
-
-use nom::{number::streaming::be_i8};
-
-fn read_fixnum(input: &[u8]) -> IResult<&[u8], i32> {
-    let (input, c) = be_i8(input)?;
-
-    if c == 0 {
-        return Ok((input, 0));
-    }
-
-    if c > 0 {
-        if c <= 127 {
-            return Ok((input, c as i32 - 5));
-        }
-
-        let (input, x) = be_i32(input)?;
-
-        return Ok((input, x));
-    } else {
-        if c >= -128 {
-            return Ok((input, c as i32 + 5));
-        }
-
-        let (input, x) = be_i32(input)?;
-
-        return Ok((input, -x));
-    }
-}
-
-fn parse_fixnum(input: &[u8]) -> IResult<&[u8], ParsedValue> {
-    let (input, value) = read_fixnum(input)?;
-    Ok((input, ParsedValue::Fixnum(value)))
+fn parsed_value_size(parsed_value: &ParsedValue) -> usize {
+    bincode::serialized_size(parsed_value).unwrap() as usize
 }
 
